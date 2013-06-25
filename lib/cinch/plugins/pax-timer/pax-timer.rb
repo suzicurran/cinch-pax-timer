29b1aa57093a3c3ffe16ec11886e8fbf60979ad2
Brian Haberer <bhaberer@gmail.com>
2013-06-25 00:36:46 -0700

    Trying to make thing a little more happy as far as CI is concerned

--- lib/cinch/plugins/pax-timer/pax-timer.rb
+++ lib/cinch/plugins/pax-timer/pax-timer.rb
@@ -28,29 +28,34 @@ module Cinch::Plugins
       end
     end
 
+    private
+
     def get_next_pax(type = nil)
+      @pax = get_next_pax_for(type)
+
+      if @pax.nil?
+        return 'I don\'t have info for the next one of those PAXes'
+      else
+        message = "#{@pax[:name]} is "
+        message << 'approximately ' if @pax[:estimated]
+
+        # Uncomment this once we can specify granularity in Time Lord.
+        # message << TimeLord::Period.new(@pax[:date], Time.now).to_words
+        message << "#{Cinch::Toolbox.time_format(@pax[:date] - Time.now, [:days])} from now"
+
+        message << " (No official date, yet)" if @pax[:estimated]
+        return message
+      end
+    end
+
+    def get_next_pax_for(type)
       @filename = File.join(File.dirname(__FILE__), "../../../../config/pax.yml")
       if File::exist?(@filename)
-        @paxes = YAML::load(File.open(@filename))
-
-        @paxes.delete_if { |pax| pax[:date] - Time.now < 0 }
-        @paxes.delete_if { |pax| pax[:type] != type } unless type.nil?
-        @paxes.sort! { |a,b| b[:date] <=> a[:date] }
-        @pax = @paxes.pop
-
-        if @pax.nil?
-          return 'I don\'t have info for the next one of those PAXes'
-        else
-          message = "#{@pax[:name]} is "
-          message << 'approximately ' if @pax[:estimated]
-
-          # Uncomment this once we can specify granularity in Time Lord.
-          # message << TimeLord::Period.new(@pax[:date], Time.now).to_words
-          message << "#{Cinch::Toolbox.time_format(@pax[:date] - Time.now, [:days])} from now"
-
-          message << " (No official date, yet)" if @pax[:estimated]
-          return message
-        end
+        paxes = YAML::load(File.open(@filename))
+        paxes.delete_if { |pax| pax[:date] - Time.now < 0 }
+        paxes.delete_if { |pax| pax[:type] != type } unless type.nil?
+        paxes.sort! { |a,b| b[:date] <=> a[:date] }
+        return paxes.last
       end
     end
   end
